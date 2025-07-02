607a02baf1562de3823cca1cc2a39a6763b82840
Jon Brown <jon@breelabs.com>
2025-07-02 13:39:08 -0400

    Workflows

---
+++ .github/scripts/update_readme.py
@@ -0,0 +1,16 @@
+START_MARKER = "<!-- VERSION_HISTORY_START -->"
+END_MARKER = "<!-- VERSION_HISTORY_END -->"
+
+with open("RELEASE_NOTES.md") as f:
+    notes = f.read()
+
+with open("README.md") as f:
+    lines = f.readlines()
+
+start = lines.index(START_MARKER + "\n")
+end = lines.index(END_MARKER + "\n")
+
+new_lines = lines[:start+1] + [notes + "\n"] + lines[end:]
+with open("README.md", "w") as f:
+    f.writelines(new_lines)
+
