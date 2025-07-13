8ee0ff82deab1e5ac2ea6a527a43e218b8dfbb70
Jon Brown <jon@jonbrown.org>
2025-07-13 16:28:28 -0400

    Added Automox Scripts

---
+++ Scripts/Automox_re_register_fix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo launchctl unload /Library/LaunchDaemons/com.automox.agent.plist
+sudo /usr/local/bin/amagent --deregister
+sudo rm -f /usr/local/bin/amagent
+sudo rm -rf "/Library/Application Support/Automox/"
+sudo /usr/bin/dscl . -delete /Users/_automoxserviceaccount
+
+sleep 5
+
+jamf policy -id $4
+
+sleep 5
+
+sudo /usr/local/bin/amagent --adminuser '$5' --adminpass '$6'
+sudo /usr/local/bin/amagent --automox-service-account enable
+sudo /usr/local/bin/amagent --automox-user-prompt enable
+
+jamf recon
+
+sysadminctl -secureTokenStatus _automoxserviceaccount
+
+/usr/local/bin/amagent --setkey $7
+launchctl load /Library/LaunchDaemons/com.automox.agent.plist
